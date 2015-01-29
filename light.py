6f39d07f86e520d0027986c60335e42c707327b3
Miles Steele <miles@milessteele.com>
2015-01-29 01:23:16 -0500

    change light.py

--- light.py
+++ light.py
@@ -1,3 +1,16 @@
+"""
+Lounge Light Controller
+
+Command Line Usage:
+	python light.py on
+	python light.py off
+
+Python Usage:
+	l = Light()
+	l.set_state(True)  # turns lights on
+	l.set_state(False) # turns lights off
+"""
+
 import os, serial, sys
 from util import path_filter
 
@@ -12,26 +25,39 @@ class Light(object):
 			print "I don't know which light controller to open"
 			sys.exit(1)
 		
-		self.light_controller = light_controller_list[0]
-		self.state = False
+		light_controller = light_controller_list[0]
+		BAUDRATE = 9600
 
-	@property
-	def state(self):
-		return self.state
+		self.serial = serial.Serial(port=light_controller, baudrate=BAUDRATE)
 
-	@state.setter
 	def set_state(self, state):
-		self.state = state
-
-		l = serial.Serial(port=light_controller, baudrate=baudrate)
-		l.write(str(int(state)))
-		l.write()
-		l.flushInput()
-		l.close()
+		""" `state` must be boolean """
+		code = {
+			True: '0',
+			False: '1',
+		}
+		self.serial.write(code[state])
+		self.serial.flushInput()
+		self.serial.close()
 
 
 if __name__ == '__main__':
-	# Example Usage:
 	l = Light()
-	l.state = False # turns lights off
-	l.state = True # turns lights on
+	YES = ['true', 'yes', 'on', '1']
+	NO = ['false', 'no', 'off', '0']
+	print "Would you like to turn the lights on or off?"
+
+	if len(sys.argv) == 2:
+		command = sys.argv[1].lower()
+		print ">", command
+	else:
+		command = raw_input("> ").lower()
+
+	if command in YES:
+		print "turning on"
+		l.set_state(True)
+	elif command in NO:
+		print "turning off"
+		l.set_state(False)
+	else:
+		print "huh?"
